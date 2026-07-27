36095c4c3db06db7f5436de749a24f9c317ea63e
ChrisLR <chrislr666@proton.me>
2026-07-27 16:04:08 -0400

    pragma

--- src/catalua_creature_filters.h
+++ src/catalua_creature_filters.h
@@ -1,12 +1,7 @@
-
-#ifndef CATACLYSMBN_CATALUA_CREATURE_FILTERS_H
-#define CATACLYSMBN_CATALUA_CREATURE_FILTERS_H
+#pragma once
 #include <vector>
 #include "sol/forward.hpp"
 
 class monster;
 
 std::vector<monster *> filter_monsters_from_lua( const sol::table &filters );
-
-
-#endif //CATACLYSMBN_CATALUA_CREATURE_FILTERS_H
